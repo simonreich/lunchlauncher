d1e1cf9c1ea2a5cd3fc9d0ca969564b01ccc244b
Simon Reich <sreich@physik3.gwdg.de>
2014-03-12 21:26:38 +0100

    added Mail-Alert Service to index.php

--- index.php
+++ index.php
@@ -58,6 +58,14 @@
 		$settext = "Lunch";
 	};
 
+	// check if there are information in the url
+	$newname = $_GET["name"];
+	$newtext = $_GET["text"];
+	if ($newname != "" && $newtext != "" )
+	{
+		$text = "${text}$timestamp;$newname;$newtext\n";
+	};
+
 	// check if there are information posted in the form
 	$newname = $_POST["name"];
 	$newtext = $_POST["text"];
@@ -74,42 +82,176 @@
 		$settext = $newtext;
 	};
 
+	// write results to file
+	file_put_contents ($file, $text, LOCK_EX);
+
+
+
+	// Mail Alert
+
+	// set global variables
+	$filemail = './maillist.dat';
+
 	// check if there are information in the url
-	$newname = $_GET["name"];
-	$newtext = $_GET["text"];
-	if ($newname != "" && $newtext != "" )
+	$newmail_GET = $_GET["mailaddress"];
+	if ($newmail_GET != "")
 	{
-		$text = "${text}$timestamp;$newname;$newtext\n";
+		$maillist = "${maillist}${newmail_GET}\n";
+	};
+
+	// check if there are information posted
+	$newmail_POST = $_POST["mailaddress"];
+	if ($newmail_POST != "")
+	{
+		$maillist = "${maillist}${newmail_POST}\n";
+	};
+
+	// check if we want to remove an email address
+	$mailaddress_remove_POST = $_POST["mailaddress_remove"];
+	$mailaddress_remove_GET = $_GET["mailaddress_remove"];
+
+	// open file
+	$handle = @fopen ($filemail, "r");
+	if ($handle)
+	{
+		// read line by line
+		while (($buffer = fgets ($handle, 4096)) !== false)
+		{
+			// check if mail address already exists
+			if (${buffer} != "${newmail_POST}\n" && ${buffer} != "${newmail_GET}\n")
+			{
+				// check if mail address wants to be removed
+				if (${buffer} != "${mailaddress_remove_POST}\n" && ${buffer} != "${mailaddress_remove_GET}\n")
+				{
+					$maillist = "${maillist}$buffer";
+				};
+			};
+		};
+
+		if (!feof($handle))
+		{
+			echo "Fehler: unerwarteter fgets() Fehlschlag\n";
+		};
+		fclose($handle);
 	};
 
 	// write results to file
-	file_put_contents ($file, $text, LOCK_EX);
+	file_put_contents ($filemail, $maillist, LOCK_EX);
+
+
+
+	// Send Mails
+
+	// set global variables
+	$fileOld = './launchlist_old.dat';
+
+	// open file
+	$handle = @fopen ($fileOld, "r");
+	if ($handle)
+	{
+		// read line by line
+		while (($buffer = fgets ($handle, 4096)) !== false)
+		{
+			// part line into array
+			$tags = explode(';', $buffer);
+
+			// check for time
+			if ((int)(($timestamp-$tags[0])/60) <= (int)$minutes)
+			{
+				$textOld = "${textOld}$tags[0];$tags[1];$tags[2]";
+			};
+		};
+
+		if (!feof($handle))
+		{
+			echo "Fehler: unerwarteter fgets() Fehlschlag\n";
+		};
+		fclose($handle);
+	};
+
+	$sendMail = "true";
+	foreach(preg_split("/((\r?\n)|(\r\n?))/", $text) as $line)
+	{
+		foreach(preg_split("/((\r?\n)|(\r\n?))/", $textOld) as $lineOld)
+		{
+			// does the string contain ';'? Basi check for emptiness of weird stuff...
+			if (strpos ($text, ';') !== false)
+			{
+				if ($text == $textOld)
+				{
+					$sendMail = "false";
+				};
+			} else {
+				$sendMail = "false";
+			};
+		};
+	};
+
+	// send the mail to the list
+	if ($sendMail == "true")
+	{
+		foreach(preg_split("/((\r?\n)|(\r\n?))/", $maillist) as $maillistLine)
+		{
+			// the foreach gives an almost empty list... So make a check for '@'
+			if (strpos ($maillistLine, '@') !== false)
+			{
+				mail ($maillistLine, "Lunch has been launched!", "Dear colleagues,\n\nthe Lunch Launcher has been launched!\n\n${newname} said: \"${newtext}\".\n\nBon appetit,\n   The Lunch Launcher");
+			}
+		};
+	};
+
+	// All alarms should have been sent by now
+	// write results to file
+	file_put_contents ($fileOld, $text, LOCK_EX);
+
 ?>
 
 <h1>Lunch Launcher</h1>
 
 <h2>Add Launch</h2>
 <form action="index.php" method="post">
-	
 	<p>Name: <input type="text" name="name" value="<?php echo $setname; ?>"/></p>
  	<p>Launch: <input type="text" name="text" value="<?php echo $settext; ?>"/></p>
 	<p><input type="submit" value="Submit" /></p>
 </form>
 
-<p></p>
-
 <h2>Full Launch List - last <?php echo $minutes?> minutes</h2>
 <?php
-	$textprint = nl2br($text);
-	$textprint = str_replace(array("\n","\r"), '', $textprint);
-	echo $textprint;
+	if ($text == "")
+	{
+		printf ("List is empty\n");
+	} else {
+		$textprint = nl2br($text);
+		$textprint = str_replace(array("\n","\r"), '', $textprint);
+		echo $textprint;
+	};
 ?> 
 
-<p></p>
+<h2>Register for Mail-Alert</h2>
+<p>Your mail address will be stored unencrypted and world readable!</p>
+<form action="index.php" method="post">
+	<p>Add Mail: <input type="text" name="mailaddress" value=""/></p>
+	<p>Remove Mail: <input type="text" name="mailaddress_remove" value=""/></p>
+	<p><input type="submit" value="Submit" /></p>
+
+<h2>Mail-Alert List</h2>
+<?php
+	if ($maillist == "")
+	{
+		printf ("List is empty\n");
+	} else {
+		$maillistprint = nl2br($maillist);
+		$maillistprint = str_replace(array("\n","\r"), '', $maillistprint);
+		$maillistprint = str_replace ("@" , " [at] " , $maillistprint);
+		$maillistprint = str_replace ('.' , ' [dot] ' , $maillistprint);
+		echo $maillistprint;
+	};
+?> 
 
 <h2>Download latest script</h2>
-<p>Latest stable version <a href="./lunchlauncher_skript.tar.gz">lunchlauncher_skript.tar.gz</a></p>
-<p>Git Repository <a href="https://fortknox.physik3.gwdg.de/gitweb/?p=lunchlauncher.git;a=summary">lunchlauncher</a></p>
+<p>Please use the public available <a href="https://fortknox.physik3.gwdg.de/gitweb/?p=lunchlauncher.git;a=summary">git Repository</a>.</p>
+
+</form>
 
 </body>
 </html>
