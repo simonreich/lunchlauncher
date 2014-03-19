decd8873ab08d76bee6e94785138eedad0969b9e
Simon Reich <sreich@physik3.gwdg.de>
2014-03-19 14:17:09 +0100

    index.php license information added

--- www/index.php
+++ www/index.php
@@ -9,6 +9,23 @@
 <body>
 
 <?php
+	// This file is part of Lunch Launcher
+	//
+	// Lunch Launcher is free software: you can redistribute it and/or modify
+	// it under the terms of the GNU General Public License as published by
+	// the Free Software Foundation, either version 3 of the License, or
+	// (at your option) any later version.
+	//
+	// Lunch Launcher is distributed in the hope that it will be useful,
+	// but WITHOUT ANY WARRANTY; without even the implied warranty of
+	// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	// GNU General Public License for more details.
+	//
+	// You should have received a copy of the GNU General Public License
+	// along with Lunch Launcher.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
 	// set global variables
 
 	// The launch list is saved here. Clients must be able to download this file.
